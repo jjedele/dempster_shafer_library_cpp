409f76da06f375ee704fec216b901e5371bd8bed
Thilo Michael <uhlomuhlo@gmail.com>
2012-03-12 20:00:13 +0100

    Added comments to test_csvreader.cpp and updated the gitignore file

--- test_csvreader.cpp
+++ test_csvreader.cpp
@@ -10,31 +10,37 @@
 #include <fstream>
 #include <stdlib.h>
 
- using namespace std;
+using namespace std;
 
- int main(int argc, char** argv) {
- 	if (argc < 2) {
- 		printf("Invalid number of arguments. Usage:\n\t %s [csv-file]", argv[0]);
- 		return EXIT_FAILURE;
- 	}
+int main(int argc, char** argv) {
+	//Check number of arguments
+	if (argc < 2) {
+		printf("Invalid number of arguments. Usage:\n\t %s [csv-file]", argv[0]);
+		return EXIT_FAILURE;
+	}
 
- 	CSVReader test(argv[1]);
- 	cout << "Number of columns: " << test.number_of_columns() << endl;
- 	cout << "Number of rows: " << test.number_of_rows() << endl;
+	//Create new instance of CSVReader using parameter provided
+	CSVReader test(argv[1]);
 
- 	cout << "Printing the Header" << endl;
- 	for (int i = 0; i < test.number_of_columns(); i++) {
- 		cout << "\tHeader " << i << ": " << test.header(i) << endl;
- 	}
+	//Output general information about the CSV file
+	cout << "Number of columns: " << test.number_of_columns() << endl;
+	cout << "Number of rows: " << test.number_of_rows() << endl;
 
- 	cout << "Printing the first row of the csv file" << endl;
+	//Printing the header (first row in the csv file)
+	cout << "Printing the header" << endl;
+	for (int i = 0; i < test.number_of_columns(); i++) {
+		cout << "\tHeader " << i << ": " << test.header(i) << endl;
+	}
 
- 	vector<int> row = test.row(0);
- 	for (int i = 0; i < test.number_of_columns(); i++) {
- 		cout << "\tColumn " << i << ": " << row.at(i) << endl;
- 	}
+	//Printing the first row of the data part of the CSV file
+	cout << "Printing the first row of the csv file" << endl;
+	vector<int> row = test.row(0);
+	for (int i = 0; i < test.number_of_columns(); i++) {
+		cout << "\tColumn " << i << ": " << row.at(i) << endl;
+	}
 
- 	cout << "Value of sixth row in column \"Frame\": " << test.value(5, "Frame") << endl;
+	//Fetching a value by it's column header and row index
+	cout << "Value of sixth row in column \"Frame\": " << test.value(5, "Frame") << endl;
 
- 	return EXIT_SUCCESS;
- }
+	return EXIT_SUCCESS;
+}
